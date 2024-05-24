2c98c1cf974ca3ebb2f9c1ab20110111eb165af5
greggy <sudakovgrigory@gmail.com>
2024-05-24 14:10:58 +0700

    main.rs: refactor

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,8 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 
-router.isReady().then(() => {
-  const route = router.currentRoute.value;
-  const searchQuery = route.query.search || '';
-  const page = parseInt(route.query.page as string, 10) || 1;
-  const sortKey = route.query.sortKey || '';
-  const sortOrder = route.query.sortOrder || 'asc';
-
-  store.dispatch('setSearchQuery', searchQuery);
-  store.dispatch('setCurrentPage', page);
-  store.dispatch('setSortKey', sortKey);
-  store.dispatch('setSortOrder', sortOrder);
-
-  store.dispatch('fetchUsers').then(() => {
-    app.mount('#app');
-  });
-});
+(async () => {
+  await router.isReady();
+  await store.dispatch('fetchUsers');
+  app.mount('#app');
+})();
